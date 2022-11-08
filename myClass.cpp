151b0a34cb8423cad1bbc42112c007d2a258d11f
Jesper Rehnfors <rehnforsjesper@gmail.com>
2022-11-08 14:35:15 +0100

    Update for myClass

--- myClass.cpp
+++ myClass.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
 class myClass {
             char name[25];
             int age;
         public:
+            void set_name(char name[25]){strncpy(this->name, name, 25);}
+            void set_age(int age) {this->age = age;}
 
             void showInfo()
             {
-                cout << "Hi my name is " << this->name << " and Im " << this->age << " old." << endl;
+                cout << "Hi my name is " << this->name << " and Im " << this->age << " years old." << endl;
             }
 
 };
@@ -16,12 +19,16 @@ class myClass {
 int main(int argc,char **argv)
 {
         myClass me;
+        char name[25];
+        int age;
 
         cout << "What is my name?" << endl;
-        cin >> me.name;
+        cin >> name;
         cout << "What is my age?" << endl;
-        cin >> me.age;
+        cin >> age;
 
+        me.set_name(name);
+        me.set_age(age);
         me.showInfo();
 
         return 0;
